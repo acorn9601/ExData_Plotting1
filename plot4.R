66a3ed0b954d576f24e1c4a9fc2ee606e22494a7
acorn9601 <jlee960101@gmail.com>
2022-01-19 21:13:11 +0900

    Code for Plot 4

---
+++ plot4.R
@@ -0,0 +1,46 @@
+# Importing dataset
+
+d_tab <- read.table("household_power_consumption.txt", sep=";", header = TRUE, na.strings = "?")
+head(d_tab)
+
+# Subsetting data from 01/02/2007 to 02/02/2007
+date_tab <- subset(d_tab, Date =="1/2/2007" | Date == "2/2/2007")
+
+# Changing Date Comlumn string to an actual Date
+date_tab$CombinedDate <- paste(date_tab$Date, date_tab$Time)
+date_tab$CombinedDate <- strptime(date_tab$CombinedDate, format = "%d/%m/%Y %H:%M:%S")
+
+# Opening an PNG file with given width/height 
+png("plot4.png", width = 480, height = 480)
+
+# Setting plot area parameters 
+par(mfrow = c(2, 2))
+
+# Adding first graph - plot 2
+with(date_tab, plot(CombinedDate, as.numeric(Global_active_power), type = "l", xlab = "", ylab = "Global Active Power (kilowatts)"))
+
+# Adding second graph
+with(date_tab, plot(CombinedDate, as.numeric(Voltage), type = "l", xlab = "datetime", ylab = "Voltage"))
+
+# Adding third graph
+with(date_tab, {
+  
+  # Plotting Sub_metering_1
+  plot(CombinedDate, Sub_metering_1, col = "black", type= "l", xlab = "", ylab = "Energy sub metering")
+  
+  # Adding line for Sub_metering_2
+  lines(CombinedDate, Sub_metering_2, col = "red")
+  
+  # Adding line for Sub_metering_3
+  lines(CombinedDate, Sub_metering_3, col = "blue")
+  
+  # Adding legend
+  legend("topright", lwd = 1, col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), bty = "n")
+}
+)
+
+# Adding fourth graph
+with(date_tab, plot(CombinedDate, as.numeric(Global_reactive_power), type = "l", xlab = "datetime", ylab = "Global_reactive_power"))
+
+# Closing device
+dev.off()
